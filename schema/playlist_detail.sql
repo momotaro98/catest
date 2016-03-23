8fa57031147b11866469199afe7a264b542f3e06
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-03-23 17:12:42 +0900

    first commit

---
+++ schema/playlist_detail.sql
@@ -0,0 +1,42 @@
+-- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
+--
+-- Host: localhost    Database: event
+-- ------------------------------------------------------
+-- Server version	5.6.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `playlist_detail`
+--
+
+DROP TABLE IF EXISTS `playlist_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playlist_detail` (
+  `playlist_name` varchar(255) NOT NULL DEFAULT '',
+  `number` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'リスト内順序',
+  `music_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`playlist_name`,`number`),
+  KEY `music_id` (`music_id`),
+  CONSTRAINT `playlist_detail_ibfk_1` FOREIGN KEY (`playlist_name`) REFERENCES `playlist` (`name`),
+  CONSTRAINT `playlist_detail_ibfk_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
